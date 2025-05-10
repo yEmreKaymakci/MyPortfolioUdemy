1e336aaa6ce46d76b059ee20f1f29fae1fc22d2f
yEmreKaymakci <yunusemrekymkci@gmail.com>
2025-05-11 02:36:03 +0300

    Add project files.

---
+++ MyPortfolioUdemy/ViewComponents/_FeatureComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioUdemy.ViewComponents
+{
+    public class _FeatureComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyPortfolioUdemy/ViewComponents/_SkillComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioUdemy.ViewComponents
+{
+    public class _SkillComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
